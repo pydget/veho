36c7fc31e08470086be7f1ba8146b45d692a99e5
hazen <hoyeungw@outlook.com>
2020-10-25 18:33:58 +0800

    minor fix

---
+++ tests/vector.swap.test.py
@@ -0,0 +1,4 @@
+from veho.vector.helper import swap
+
+vec = ['a', 'b', 'c', 'd', 'e']
+print(swap(vec, 0, 2))
--- veho/vector/__init__.py
+++ veho/vector/__init__.py
@@ -4,3 +4,4 @@ from .mapper import iterate, mapper, mutate, shallow
 from .margin import margin_mapper, margin_mutate, margin_shallow
 from .sparse_list import SparseList
 from .zipper import duozipper, mutazip, quazipper, to_duozipper, to_quazipper, to_trizipper, trizipper, zipper
+from .helper import swap
